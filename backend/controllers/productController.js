bb6d0850cb4f46934c128fb5785434e5a5374b2d
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-12-18 22:10:34 +0530

    added route to update product by id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -36,3 +36,29 @@ exports.getSingleProduct = async (req, res, next) => {
 		product,
 	});
 };
+
+// Update Product => /api/v1/product/:id
+exports.updateProduct = async (req, res, next) => {
+	const product = await Product.findById(req.params.id);
+
+	if (!product) {
+		return res.status(404).json({
+			success: false,
+			message: 'product not found',
+		});
+	}
+
+	const updatedProduct = await Product.findByIdAndUpdate(
+		req.params.id,
+		req.body,
+		{
+			new: true,
+			runValidators: true,
+		},
+	);
+
+	res.status(200).json({
+		success: true,
+		product: updatedProduct,
+	});
+};
