7e54fffb9b01922398a6645ca84cb7f1234a2f95
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-12-18 19:38:04 +0530

    added new route to create a croduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,15 @@
+const Product = require('../models/product');
+
+// Create new product => /api/v1/product/new
+exports.newProduct = async (req, res, next) => {
+	const product = await Product.create(req.body);
+	res.status(201).json({
+		success: true,
+		product,
+	});
+};
+
+// Get products list => /api/v1/products
 exports.getProducts = (req, res, next) => {
 	console.log('Ek request ayaa!');
 	res.status(200).json({
